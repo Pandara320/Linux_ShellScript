aed66b5aa7c9cd0181853d9d87ff929a9346a22a
Pandara320 <31490140+Pandara320@users.noreply.github.com>
2020-10-12 13:46:37 +0800

    Add files via upload

---
+++ python-achieved/py-cs1/list8_1_2.py
@@ -0,0 +1,12 @@
+friends = ['Marx', 'Frank','Jerry','Allen']
+for myFriend in friends :
+    print ('Happy New Year:', myFriend)
+for a in range(len(friends)) :
+    myFriend = friends[a]
+    print ('This is another Happy New Year:', myFriend)
+myStuff = list()
+myStuff.append('Game')
+myStuff.append('girlFriend')
+myStuff.append('boyFriend')
+myStuff.append('520')
+print(myStuff)
